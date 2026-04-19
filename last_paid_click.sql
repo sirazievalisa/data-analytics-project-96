6a20b005768b6ca7c975afcd3f0fdc6201403f8b
sirazievalisa <sirazievalisa@gmail.com>
2026-04-19 20:45:50 +0300

    last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,88 +1,52 @@
-WITH paid_sessions AS (
-    SELECT
+WITH last_paid_click AS (
+    SELECT DISTINCT ON (s.visitor_id)
         s.visitor_id,
         s.visit_date,
         s.source AS utm_source,
         s.medium AS utm_medium,
         s.campaign AS utm_campaign
     FROM sessions AS s
-    WHERE LOWER(s.medium) IN (
-        'cpc',
-        'cpm',
-        'cpa',
-        'youtube',
-        'cpp',
-        'tg',
-        'social'
-    )
+    WHERE LOWER(s.medium) != 'organic'
+    ORDER BY
+        s.visitor_id,
+        s.visit_date DESC
 ),
-
-ranked_attribution AS (
-    SELECT
-        ps.visitor_id,
-        ps.visit_date,
-        ps.utm_source,
-        ps.utm_medium,
-        ps.utm_campaign,
+attributed_leads AS (
+    SELECT DISTINCT ON (l.lead_id)
+        lpc.visitor_id,
+        lpc.visit_date,
+        lpc.utm_source,
+        lpc.utm_medium,
+        lpc.utm_campaign,
         l.lead_id,
         l.created_at,
         l.amount,
         l.closing_reason,
-        l.status_id,
-        ROW_NUMBER() OVER (
-            PARTITION BY l.lead_id
-            ORDER BY
-                ps.visit_date DESC
-        ) AS rn
-    FROM paid_sessions AS ps
+        l.status_id
+    FROM last_paid_click AS lpc
     LEFT JOIN leads AS l
-        ON
-            ps.visitor_id = l.visitor_id
-            AND
-            ps.visit_date <= l.created_at
-),
-
-attributed_leads AS (
-    SELECT
-        ra.visitor_id,
-        ra.visit_date,
-        ra.utm_source,
-        ra.utm_medium,
-        ra.utm_campaign,
-        ra.lead_id,
-        ra.created_at,
-        ra.amount,
-        ra.closing_reason,
-        ra.status_id
-    FROM ranked_attribution AS ra
-    WHERE
-        ra.lead_id IS NOT NULL
-        AND
-        ra.rn = 1
+        ON lpc.visitor_id = l.visitor_id
+       AND lpc.visit_date <= l.created_at
+    ORDER BY
+        l.lead_id,
+        l.created_at ASC
 )
 
 SELECT
-    ps.visitor_id,
-    ps.visit_date,
-    ps.utm_source,
-    ps.utm_medium,
-    ps.utm_campaign,
+    al.visitor_id,
+    al.visit_date,
+    al.utm_source,
+    al.utm_medium,
+    al.utm_campaign,
     al.lead_id,
     al.created_at,
     al.amount,
     al.closing_reason,
     al.status_id
-FROM paid_sessions AS ps
-LEFT JOIN attributed_leads AS al
-    ON
-        ps.visitor_id = al.visitor_id
-        AND ps.visit_date = al.visit_date
-        AND ps.utm_source = al.utm_source
-        AND ps.utm_medium = al.utm_medium
-        AND ps.utm_campaign = al.utm_campaign
+FROM attributed_leads AS al
 ORDER BY
     al.amount DESC NULLS LAST,
-    ps.visit_date ASC,
-    ps.utm_source ASC,
-    ps.utm_medium ASC,
-    ps.utm_campaign ASC;
+    al.visit_date ASC,
+    al.utm_source ASC,
+    al.utm_medium ASC,
+    al.utm_campaign ASC;
