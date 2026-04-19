3256b0463b47ddbcee3148126b799756341de714
sirazievalisa <sirazievalisa@gmail.com>
2026-04-19 15:08:44 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,12 +1,11 @@
 WITH paid_sessions AS (
     SELECT
         s.visitor_id,
-        s.visit_date::date AS visit_date,
+        s.visit_date,
         s.source AS utm_source,
         s.medium AS utm_medium,
         s.campaign AS utm_campaign
-    FROM
-        sessions AS s
+    FROM sessions AS s
     WHERE LOWER(s.medium) IN (
         'cpc',
         'cpm',
@@ -34,25 +33,29 @@ ranked_attribution AS (
             PARTITION BY l.lead_id
             ORDER BY ps.visit_date DESC
         ) AS rn
-    FROM
-        paid_sessions AS ps
-    LEFT JOIN
-        leads AS l
-        ON
-            ps.visitor_id = l.visitor_id
-            AND ps.visit_date <= l.created_at::date
+    FROM paid_sessions AS ps
+    LEFT JOIN leads AS l
+        ON ps.visitor_id = l.visitor_id
+        AND ps.visit_date <= l.created_at
 ),
 
 attributed AS (
-    SELECT *
-    FROM
-        ranked_attribution
-    WHERE
-        lead_id IS NOT NULL
-        AND rn = 1
+    SELECT
+        ra.visitor_id,
+        ra.visit_date,
+        ra.utm_source,
+        ra.utm_medium,
+        ra.utm_campaign,
+        ra.lead_id,
+        ra.created_at,
+        ra.amount,
+        ra.closing_reason,
+        ra.status_id
+    FROM ranked_attribution AS ra
+    WHERE ra.lead_id IS NOT NULL
+      AND ra.rn = 1
 ),
 
--- расходы из VK
 vk_costs AS (
     SELECT
         campaign_date::date AS visit_date,
@@ -60,13 +63,10 @@ vk_costs AS (
         utm_medium,
         utm_campaign,
         SUM(daily_spent) AS total_cost
-    FROM
-        vk_ads
-    GROUP BY
-        1, 2, 3, 4
+    FROM vk_ads
+    GROUP BY 1, 2, 3, 4
 ),
 
--- расходы из Яндекс
 ya_costs AS (
     SELECT
         campaign_date::date AS visit_date,
@@ -74,10 +74,8 @@ ya_costs AS (
         utm_medium,
         utm_campaign,
         SUM(daily_spent) AS total_cost
-    FROM
-        ya_ads
-    GROUP BY
-        1, 2, 3, 4
+    FROM ya_ads
+    GROUP BY 1, 2, 3, 4
 ),
 
 all_costs AS (
@@ -92,61 +90,55 @@ costs_agg AS (
         utm_source,
         utm_medium,
         utm_campaign,
-        SUM(cost) AS total_cost
-    FROM
-        all_costs
-    GROUP BY
-        1, 2, 3, 4
+        SUM(total_cost) AS total_cost
+    FROM all_costs
+    GROUP BY 1, 2, 3, 4
 )
+
 SELECT
-    ps.visit_date,
+    ps.visit_date::date AS visit_date,
+    COUNT(DISTINCT ps.visitor_id) AS visitors_count,
     ps.utm_source,
     ps.utm_medium,
     ps.utm_campaign,
     c.total_cost,
-    COUNT(*) AS visitors_count,
+    COUNT(DISTINCT a.lead_id) AS leads_count,
     COUNT(
-        a.lead_id
-    ) AS leads_count,
-    COUNT(
-        CASE
-            WHEN
-                a.closing_reason = 'Успешно реализовано'
+        DISTINCT CASE
+            WHEN a.closing_reason = 'Успешно реализовано'
                 OR a.status_id = 142
-                THEN 1
+            THEN a.lead_id
         END
     ) AS purchases_count,
     SUM(
         CASE
-            WHEN
-                a.closing_reason = 'Успешно реализовано'
+            WHEN a.closing_reason = 'Успешно реализовано'
                 OR a.status_id = 142
-                THEN a.amount
+            THEN a.amount
+            ELSE NULL
         END
     ) AS revenue
-FROM
-    paid_sessions AS ps
-LEFT JOIN
-    attributed AS a
-    ON
-        ps.visitor_id = a.visitor_id
-        AND ps.visit_date = a.visit_date
-LEFT JOIN
-    costs_agg AS c
-    ON
-        ps.visit_date = c.visit_date
-        AND ps.utm_source = c.utm_source
-        AND ps.utm_medium = c.utm_medium
-        AND ps.utm_campaign = c.utm_campaign
+FROM paid_sessions AS ps
+LEFT JOIN attributed AS a
+    ON ps.visitor_id = a.visitor_id
+    AND ps.visit_date = a.visit_date
+    AND ps.utm_source = a.utm_source
+    AND ps.utm_medium = a.utm_medium
+    AND ps.utm_campaign = a.utm_campaign
+LEFT JOIN costs_agg AS c
+    ON ps.visit_date::date = c.visit_date
+    AND ps.utm_source = c.utm_source
+    AND ps.utm_medium = c.utm_medium
+    AND ps.utm_campaign = c.utm_campaign
 GROUP BY
-    ps.visit_date,
+    ps.visit_date::date,
     ps.utm_source,
     ps.utm_medium,
     ps.utm_campaign,
     c.total_cost
 ORDER BY
     revenue DESC NULLS LAST,
-    ps.visit_date ASC,
+    ps.visit_date::date ASC,
     visitors_count DESC,
     ps.utm_source ASC,
     ps.utm_medium ASC,
